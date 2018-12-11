a35e1c7317a743881ed693cadd99a5c7ed767361
goodpineapple <taemin3000@naver.com>
2018-12-11 15:35:50 +0900

    with father first example

---
+++ test1.R
@@ -0,0 +1,18 @@
+# p.15
+# shiny패키지 다운로드
+install.packages("shiny")
+library(shiny)
+
+# 예제실행 
+# http://127.0.0.1:3970/ 확인 URL
+runExample("01_hello")      # a hisdtogram 
+runExample("02_text")       # tables and data frames
+runExample("03_reactivity") # a reactive expression
+runExample("04_mpg")        # global variables
+runExample("05_sliders")    # slider bars
+runExample("06_tabsets")    # tabbed panels
+runExample("07_widgets")    # help text and submit buttons
+runExample("08_html")       # Shiny app built from HTML
+runExample("09_upload")     # file upload wizard
+runExample("10_download")   # file download wizard
+runExample("11_timer")      # an automated timer
